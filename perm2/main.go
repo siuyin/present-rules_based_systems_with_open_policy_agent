74bc928963a4fb08de9d07e4b4d96a374e10eed6
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-08-14 06:17:43 +0000

    Added comments to perm2.

--- perm2/main.go
+++ perm2/main.go
@@ -1,5 +1,6 @@
 package main
 
+// 10 OMIT
 import (
 	"context"
 	"fmt"
@@ -7,6 +8,7 @@ import (
 	"github.com/open-policy-agent/opa/rego"
 )
 
+// 20 OMIT
 type BuyFlatApplication struct {
 	Applicants []Applicant
 }
@@ -18,36 +20,41 @@ type ApplicationDecision struct {
 	Approved bool
 }
 
+// 30 OMIT
 func main() {
 	fmt.Println("Closer to the real HDB")
 	app := BuyFlatApplication{[]Applicant{
 		Applicant{ResidentStatus: "pr"},
-		Applicant{ResidentStatus: "citizen"},
+		Applicant{ResidentStatus: "citizen"}, // change me
 		Applicant{ResidentStatus: "other"},
 	}}
 	decision := OKToBuyFlat(&app)
-	fmt.Printf("Your application result is: %v\n", decision)
+	fmt.Printf("Your application result is: %#v\n", decision)
 }
 
+// 40 OMIT
 func OKToBuyFlat(app *BuyFlatApplication) ApplicationDecision {
-	fmt.Printf("%#v\n", *app)
-	rego := rego.New(
+	rego := rego.New( // explain this structure
 		rego.Query("data.hdb"),
 		rego.Input(app),
-		rego.Module("hdb_criteria.rego",
-			` package hdb
+		rego.Module("hdb_criteria.rego", `
+package hdb
 default atLeastOneCitizen = false
 
 atLeastOneCitizen {
 	input.Applicants[_].ResidentStatus == "citizen"
 }
 `))
-	rs, err := rego.Eval(context.Background()) // rs: result set
+	rs, err := rego.Eval(context.Background()) // rs: result set, has Expressions and Bindings
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	var d ApplicationDecision
-	d.Approved = rs[0].Expressions[0].Value.(map[string]interface{})["atLeastOneCitizen"].(bool)
-	fmt.Println(d)
+	d.Approved = rs[0]. // talk about interface{} and type assertions
+				Expressions[0].
+				Value.(map[string]interface{})["atLeastOneCitizen"].(bool)
 	return d
 }
+
+// 50 OMIT
